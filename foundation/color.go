f96d73f5a5b715eba78516844e131e7d631fa2d1
Tarcisio Oliveira <tarolive@redhat.com>
2023-03-12 16:02:21 -0300

    update

---
+++ foundation/color.go
@@ -0,0 +1,14 @@
+package foundation
+
+type BrandColor struct {
+	Primary   string
+	Secondary string
+}
+
+type BackgroundColor struct {
+	Primary       string
+	Secondary     string
+	SecondaryGray string
+	Tertiary      string
+	TertiaryGray  string
+}
